781de70eafe783666eb7f9ceeeba92dfa1c36f58
henriqueacf <henrique.assis145@gmail.com>
2023-07-24 11:27:01 -0300

    feat: add ngClass

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -17,6 +17,11 @@ export class ListarPensamentoComponent implements OnInit {
       conteudo: 'teste 3',
       autoria: 'minha tbm',
       modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris condimentum at risus non convallis. Maecenas semper sem a lacinia cursus. Ut varius a lectus quis eleifend. Nunc mollis orci eu arcu scelerisque volutpat. Mauris et magna ut nunc luctus auctor eleifend eu urna. Vivamus id tristique risus. Nam ac tincidunt augue. Vivamus in tristique felis, at condimentum neque. Aenean massa libero, vulputate sed dapibus nec, pharetra sit amet odio. Cras eros turpis, facilisis vitae varius vel, elementum a sapien. Praesent sit amet tellus luctus, pretium ante maximus, vulputate tortor. Nulla sit amet rhoncus mauris. Cras hendrerit pellentesque mauris, nec scelerisque odio. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      autoria: 'minha tbm',
+      modelo: 'modelo2'
     }
   ]
   constructor() { }
